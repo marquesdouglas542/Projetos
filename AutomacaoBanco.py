58e4a4a1ef6e9d1d70acac97a4d44e507727d2cb
marquesdouglas542 <marquesdouglas542@gmail.com>
2024-12-26 17:59:46 -0300

    Melhorias na forma de mapeamento, agora o mapeamento da tela é feito com base nas imagens

--- AutomacaoBanco.py
+++ AutomacaoBanco.py
@@ -1,6 +1,17 @@
 import pyautogui
 import time
+import os
 from cryptography.fernet import Fernet
+
+# Definindo o diretório das imagens
+diretorio_imagens = os.path.join(os.getcwd(), 'posicoesPorImagem')
+
+# Função para obter o caminho completo da imagem
+def obter_caminho_imagem(nome_imagem):
+    return os.path.join(diretorio_imagens, nome_imagem)
+
+relatorio = 'PSG_Relatorio com itens - (0097)'
+salvar_relatorio_no_diretorio = '\\\\tsclient\\C\\Users\\JFC\\Desktop\\Relatorios_Psg'
 
 # Tempo de aguardo entre cada ação
 pyautogui.PAUSE = 0.3
@@ -22,87 +33,120 @@ s_Amb = cipher_suite.decrypt(HYxdPwd).decode()
 u = cipher_suite.decrypt(JPmdC).decode()
 s = cipher_suite.decrypt(TWyAA).decode()
 
-# Definindo o diretório e relatório
-relatorio = "PSG_Relatorio com itens - (0097)"
-diretorio = "\\\\tsclient\\C\\Users\\JFC\\Desktop\\Relatorios_Psg"  # Mudar conforme necessário
-
 def abrir_sap():
-    pyautogui.click(x=1362, y=751)  # Garantir que vai estar na área de trabalho
-    pyautogui.moveTo(x=555, y=25, duration=1)  # Pegar posição do SAP na desktop (Alterar conforme necessário)
-    pyautogui.click(x=555, y=25)  # Clicar no ícone (alterar conforme necessário)
-    pyautogui.press("enter")  # Para abrir o SAP
-    time.sleep(5)
+    pyautogui.hotkey('winleft', 'd')  # Garantir que vai estar na área de trabalho
+    time.sleep(1)   
+    iconeSAP = pyautogui.locateOnScreen(obter_caminho_imagem('iconeSAP.png'), confidence=0.8)
+    pyautogui.moveTo(iconeSAP)
+    pyautogui.click()
+    pyautogui.hotkey('enter')
 
 def logar_sap(s_Amb):
-    pyautogui.moveTo(x=490, y=225, duration=1)  # Move o mouse até o campo de s
-    pyautogui.click(x=490, y=225)  # Entra no campo de s
-    pyautogui.write(s_Amb)  # Digita a s do ambiente (alterar conforme necessário)
-    pyautogui.press("enter")  # Loga no ambiente
-    time.sleep(15)  # Aguarda 15 segundos para logar no usuário, garantindo que o ambiente conectou
-
-def logar_usuario(u, s):
-    pyautogui.moveTo(x=793, y=364, duration=1)
-    pyautogui.click(x=793, y=364)
-    pyautogui.write(u)  # Usuário, mudar conforme necessário
-    pyautogui.press("tab")  # Muda para o campo de s
-    pyautogui.write(s)  # s do usuário, trocar conforme necessário
-    pyautogui.press("enter")
-    time.sleep(10)
-
-def fechar_janela_selecionar_filial():
-    pyautogui.moveTo(x=515, y=425, duration=1)
-    pyautogui.click(x=515, y=425)
-    pyautogui.press("esc")  # Fecha outra possível janela que abre
-    time.sleep(5)
-
-def abrir_gerenciador_consultas(relatorio):
-    pyautogui.moveTo(x=860, y=53, duration=1)
-    pyautogui.click(x=860, y=53)
-    pyautogui.moveTo(x=632, y=264, duration=1)  # Campo de pesquisa
-    pyautogui.click(x=632, y=264)
-    pyautogui.write(relatorio)  # Nome da consulta (Alterar conforme necessário)
-    pyautogui.moveTo(x=1011, y=334, duration=1)  # Início da barra de rolagem
-    pyautogui.mouseDown(button="left", x=1010, y=478)
-    time.sleep(5)
-    pyautogui.moveTo(x=430, y=466, duration=1)
-    pyautogui.click(x=430, y=466)
-    pyautogui.press("enter")
-    time.sleep(5)
-
-def converter_xlsx(diretorio):
-    pyautogui.moveTo(x=166, y=55, duration=1)
-    pyautogui.click(x=166, y=55)
-    time.sleep(5)
-    pyautogui.moveTo(x=893, y=161, duration=1)
-    pyautogui.click(x=893, y=161)
-    pyautogui.write(diretorio)
-    time.sleep(5)
-    pyautogui.press("enter")
-    time.sleep(5)
-    pyautogui.moveTo(x=614, y=509, duration=1)
-    pyautogui.click(x=614, y=509)
-    pyautogui.moveTo(x=565, y=564, duration=1)
-    pyautogui.click(x=565, y=564)
-    time.sleep(5)
-    pyautogui.press("enter")
-    pyautogui.moveTo(x=422, y=381, duration=1)
-    pyautogui.click(x=422, y=381)
-    time.sleep(3)
-    pyautogui.press("enter")
-    time.sleep(6)
-    pyautogui.click(x=681, y=317)
-    pyautogui.press("esc")
+    time.sleep(1)
+    ambiente = pyautogui.locateOnScreen(obter_caminho_imagem('s_Amb.png'), confidence=0.8)
+    pyautogui.moveTo(ambiente)
+    pyautogui.click()
+    pyautogui.write(s_Amb)
+    pyautogui.hotkey('enter')
     
-def fechar_relatorio():
+def logar_usuario(u, s):
+    time.sleep(15)
+    logar = pyautogui.locateOnScreen(obter_caminho_imagem('loginSAP.png'), confidence=0.8)
+    pyautogui.moveTo(logar)
+    pyautogui.click()
+    pyautogui.write(u)
+    
+    time.sleep(2)
+    
+    pyautogui.hotkey('tab')
+    pyautogui.write(s)
+    pyautogui.hotkey('enter')
+    
+    time.sleep(10)
+    
+    Bcancelar = pyautogui.locateOnScreen(obter_caminho_imagem('botaoCancelar.png'), confidence=0.8)
+    pyautogui.moveTo(Bcancelar)
+    
     time.sleep(3)
-    pyautogui.click(x=681, y=317)
-    pyautogui.press("esc")
+    
+    pyautogui.click(button='left')
+    
+    pyautogui.hotkey('esc')
+  
+def abrir_consulta_formularios():
+    time.sleep(3)
+    icon_formulario = pyautogui.locateOnScreen(obter_caminho_imagem('iconeFormularios.png'), confidence=0.9)
+    pyautogui.moveTo(icon_formulario)
+    pyautogui.click()
+    time.sleep(3)
+    
+    campo_formulario = pyautogui.locateOnScreen(obter_caminho_imagem('campoFormulario.png'), confidence=0.9)
+    pyautogui.click(campo_formulario)
+    pyautogui.write(relatorio)
+    pyautogui.hotkey("enter") 
+    
+    pyautogui.moveTo(x=1011, y=334)  # Início da barra de rolagem
+    pyautogui.mouseDown(button="left", x=1010, y=478)
+    time.sleep(1)
+    pyautogui.moveTo(x=430, y=466)
+    
+    nome_relatorio = pyautogui.locateOnScreen(obter_caminho_imagem('PSG_Relatorio com itens - (0097).png'), confidence=0.85)
+    
+    if nome_relatorio == relatorio.strip('.png'): 
+        pyautogui.moveTo(nome_relatorio)
+        pyautogui.click(button='left')
+        pyautogui.press("enter")
+        time.sleep(3)
+    else: 
+        pyautogui.moveTo(nome_relatorio)
+        pyautogui.click(button='left')
+        pyautogui.press("enter")
+        time.sleep(3)
+        
+def salvar_relatorio_xlsx():
+    botao_converter = pyautogui.locateOnScreen(obter_caminho_imagem('converterXLSX.png'), confidence=0.9)
+    pyautogui.moveTo(botao_converter)
+    pyautogui.click()
+    
+    time.sleep(2)
+    
+    mudar_diretorio = pyautogui.locateOnScreen(obter_caminho_imagem('MudarDiretorio.png'), confidence=0.9)
+    pyautogui.moveTo(mudar_diretorio)
+    pyautogui.click(button='right')
+    botao_edit_address = pyautogui.locateOnScreen(obter_caminho_imagem('botaoEditAddress.png'), confidence=0.9)
+    pyautogui.moveTo(botao_edit_address)
+    pyautogui.click(button='left')
+    pyautogui.write(salvar_relatorio_no_diretorio)
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+    
+    selecionar_tipo_arquivo = pyautogui.locateOnScreen(obter_caminho_imagem('barraDeSelecaoTipoArquivo.png'), confidence=0.9)
+    pyautogui.moveTo(selecionar_tipo_arquivo)
+    pyautogui.click()
+    time.sleep(1)
+    
+    tipo_xlsx = pyautogui.locateOnScreen(obter_caminho_imagem('tipoXLSX.png'), confidence=0.9)
+    pyautogui.moveTo(tipo_xlsx)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.hotkey('enter')
+    
+    pyautogui.moveTo(x=422, y=391)
+    time.sleep(2)
+    pyautogui.click(button='left', clicks=2)
+    time.sleep(1)
+    pyautogui.press('esc')
+    fechar_janela_final = pyautogui.locateOnScreen(obter_caminho_imagem('fecharTelaFinal.png'), confidence=0.9)
+    pyautogui.moveTo(fechar_janela_final)
+    pyautogui.click()
+    pyautogui.press('esc')
+    time.sleep(3)
+    abrir_consulta_formularios()
+    salvar_relatorio_xlsx()
 
 if __name__ == "__main__":
     abrir_sap()
     logar_sap(s_Amb)
     logar_usuario(u, s)
-    fechar_janela_selecionar_filial()  # Dependendo do usuário, esse trecho pode ser comentado
-    abrir_gerenciador_consultas(relatorio)
-    converter_xlsx(diretorio)
-    fechar_relatorio()
+    abrir_consulta_formularios()
+    salvar_relatorio_xlsx()
--- auxiliar.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-
-pyautogui.PAUSE =0.3
-time.sleep(5)
-print(pyautogui.position())
